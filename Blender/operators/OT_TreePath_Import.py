88560c7f6901e567f4f862fa9a4cc63d23937df6
GreenTrafficLight <tailgunnerpursuit@gmail.com>
2025-07-26 03:31:10 +0200

    Changes for reading path and textures

--- Blender/operators/OT_TreePath_Import.py
+++ Blender/operators/OT_TreePath_Import.py
@@ -1,6 +1,9 @@
 import bpy
 
 from ...Formats.EFO.efoFile import *
+from ...Formats.pa8 import *
+
+from ..utils.ImportEFO import *
 
 class IDAS_OT_TreePath_Import(bpy.types.Operator):
         bl_idname = "import_idas.treepathimport_operator"
@@ -10,5 +13,14 @@ class IDAS_OT_TreePath_Import(bpy.types.Operator):
             props = context.scene.tree_path_importer_properties
 
             treeModel = EFO(props.tree_model_file)
+            treeName = props.tree_model_file.split("\\")[-1]
+            
+            if props.import_textures:
+                treeNameHead = os.path.split(props.tree_model_file)[0]
+                common_texture_dir = treeNameHead + "\\" + treeName[:-4] + "_" + "textures\\"
+
+            build_hierarchy(treeModel, common_texture_dir, os.path.splitext(treeName)[0])
+
+            treePath = PA(props.tree_path_file)
 
             return {'FINISHED'}
